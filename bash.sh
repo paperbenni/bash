3baba6cbb79431eafa1332af27fc2af7fc1cc5c8
paperbenni <paperbenni@gmail.com>
2018-07-16 14:32:38 +0200

    filetype
    added filetype function to check for existance of filetype in the current dir

--- bash.sh
+++ bash.sh
@@ -20,3 +20,22 @@ mkcd() {
         mkdir "$1" || echo "dir already exists"
         cd "$1" || echo "problem creating the dir"
 }
+
+pjava() {
+        if [ -e ./"$1" ]
+        then
+                java -Xmx650m -Xms650m -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar "$1"
+        else
+                echo "file not existing, trying out other jar files!"
+                pjava ./*.jar
+        fi
+}
+
+filetype() {
+        ls ./*."$1"
+        if [ $1 -eq 0 ]
+                return 0
+        else
+                return 1
+        fi
+}
