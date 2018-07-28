77a1c421192cdff2d01e5e3bfbd45953c7f84e3e
paperbenni <paperbenni@gmail.com>
2018-07-28 13:30:44 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+if [ -e .bashfunctions.sh ]
+then
+  source .bashfunctions.sh
+else
+  curl https://raw.githubusercontent.com/paperbenni/bash/master/bash.sh > .bashfunctions.sh
+  echo "paperbenni's bash tools successfully installed!"
+  source .bashfunctions.sh
+fi
