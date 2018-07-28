a8db16f55cf3e6fdc35fdf19a80ac15d5836f7c8
paperbenni <paperbenni@gmail.com>
2018-07-28 11:22:20 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -12,6 +12,12 @@ gitexe() {
         curl https://raw.githubusercontent.com/paperbenni/"$1"/master/"$2".sh | bash
 }
 
+gitsource(){
+        curl https://raw.githubusercontent.com/paperbenni/"$1"/master/"$2".sh > gitsource.sh
+        source gitsource.sh
+        rm gitsrouce.sh
+}
+
 gitget() {
         curl https://raw.githubusercontent.com/paperbenni/"$1"/master/"$2".sh
 }
