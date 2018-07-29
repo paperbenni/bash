1cb435903f00200f6dda5b793f58ea1868363333
paperbenni <paperbenni@gmail.com>
2018-07-29 13:52:36 +0200

    set up github pages and write welcome message

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if grep -q paperbennibash01 ~/.bashrc; then
-  echo "already installed"
-else
-  echo paperbennibash01 >> .bashrc
-  echo "sucessfully installed paperbenni's bash tools!"
-  echo "$(curl https://raw.githubusercontent.com/paperbenni/bash/master/setup.sh)\n$(cat .bashrc)" > .bashrc
-  echo "appended functions"
-fi
--- setup.sh
+++
@@ -1,8 +0,0 @@
-if [ -e .bashfunctions.sh ]
-then
-  source .bashfunctions.sh
-else
-  curl https://raw.githubusercontent.com/paperbenni/bash/master/bash.sh > .bashfunctions.sh
-  echo "paperbenni's bash tools successfully installed!"
-  source .bashfunctions.sh
-fi
