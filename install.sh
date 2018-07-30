29cd563ca59d0d1dddd583b1b02870cd49e30113
paperbenni <paperbenni@gmail.com>
2018-07-30 15:13:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-if grep -q paperbennibash01 ~/.bashrc; then
-  echo "already installed"
-else
+install(){
   echo "#paperbennibash01" >> .bashrc
   echo "sucessfully installed paperbenni's bash tools!"
   echo "$(curl https://raw.githubusercontent.com/paperbenni/bash/master/setup.sh) >> .bashrc
   echo "appended functions"
+}
+
+if [ -e ~/.bashrc ]
+then
+  echo "bashrc found"
+else
+  touch .bashrc
+fi
+
+if grep -q paperbennibash01 ~/.bashrc; then
+  echo "already installed"
+else
+  install
 fi
