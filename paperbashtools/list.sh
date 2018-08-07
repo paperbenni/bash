1b849f04608bc75b4abf695dacf0ad37472d541d
paperbenni <paperbenni@gmail.com>
2018-08-07 16:34:02 +0200

    fixed paperbashtools

--- paperbashtools/list.sh
+++ paperbashtools/list.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 paperbashbuild() {
+	if [ -e packages.paperbash ]
+	then
+		rm packages.paperbash
+	fi
 	for THISFILE in $(find . -type f -not -path "*.git/*"); do
 		THISLINE=${THISFILE:2}
 		echo $THISLINE >> packages.paperbash
