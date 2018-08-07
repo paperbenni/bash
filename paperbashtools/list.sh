ebb6d11231ca8928278b8acd0dddfafb24ac1fc3
paperbenni <paperbenni@gmail.com>
2018-08-07 16:18:26 +0200

    updated packages.paperbash

--- paperbashtools/list.sh
+++ paperbashtools/list.sh
@@ -2,6 +2,6 @@
 paperbashbuild() {
 	for THISFILE in $(find . -type f -not -path "*.git/*"); do
 		THISLINE=${THISFILE:2}
-		echo $THISLINE >>packages.paperbash
+		echo $THISLINE >> packages.paperbash
 	done
 }
