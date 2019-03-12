c054e6d24cdca1ef0006b0ad02a92956a5dc8a04
Paper Benni <paperbenni@gmail.com>
2019-03-12 16:06:07 +0100

    ngrok dir fix

--- ngrok/ngrok.sh
+++ ngrok/ngrok.sh
@@ -6,7 +6,7 @@ exegrok() {
     else
         if ! ~/ngrok/ngrok --version; then
             mkdir -p ~/ngrok
-            pushd ngrok
+            pushd ~/ngrok
             wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
             chmod +x ngrok
             if ! ./ngrok --version; then
@@ -24,7 +24,7 @@ rungrok() {
     mkdir -p ~/.ngrok2
 
     while true; do
-        curl "https://raw.githubusercontent.com/paperbenni/ngrok-docker/master/tokens.txt" >./ngroktokens.txt
+        curl "https://raw.githubusercontent.com/paperbenni/bash/master/ngrok/tokens.txt" >./ngroktokens.txt
         exegrok authtoken $(shuf -n 1 ./ngroktokens.txt)
         rm ./ngroktokens.txt
         sleep 5
