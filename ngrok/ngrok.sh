ca8154c40fb03aec323961921b14ddb1c54f03ea
Paper Benni <paperbenni@gmail.com>
2019-03-14 23:25:49 +0100

    new getgrok

--- ngrok/ngrok.sh
+++ ngrok/ngrok.sh
@@ -38,6 +38,5 @@ rungrok() {
 }
 
 getgrok() {
-    echo "getting ngrok adress..."
-    curl -s localhost:4040/inspect/http | grep -oP 'window.common[^;]+' | sed 's/^[^\(]*("//' | sed 's/")\s*$//' | sed 's/\\"/"/g' | jq -r ".Session.Tunnels | values | map(.URL) | .[]" | grep "^tcp://" | sed 's/tcp\?:\/\///' >ngrokadress.txt
+    curl localhost:4040/api/tunnels | grep -oP 'tcp://.*?:[0-9]*'
 }
