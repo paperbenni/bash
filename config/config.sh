96ec84bd7c7e50120ebf8d1da667d0f8bf77169c
paperbenni <paperbenni@gmail.com>
2019-04-03 18:47:41 +0200

    added confset

---
+++ config/config.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function confset() {
+
+    if ! [ -e "$1" ]; then
+        echo "target config file $1 not existing!"
+        return 1
+    fi
+    if [ -z "$3" ]; then
+        echo "usage: confset file option value"
+    fi
+    if grep "$2=$3" <"$1"; then
+        echo "value $2=$3 already set"
+        return 0
+    else
+        echo "setting $2=$3"
+    fi
+    NEWVALUE="$2=$3"
+    sed -i "/^$2=/c $NEWVALUE" "$1"
+
+}
