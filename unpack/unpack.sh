712ac4d853dea180f473f51367a5f126f12615f6
paperbenni <paperbenni@gmail.com>
2019-04-18 14:20:32 +0200

    fix unpack

--- unpack/unpack.sh
+++ unpack/unpack.sh
@@ -2,11 +2,12 @@
 
 #automatically determines filetype and extracts the archive
 unpack() {
-    if [ -z "$@" ]; then
+    if [ -z "$1" ]; then
         echo "usage: unpack file
       automatically detects the archive type and extracts it"
         return
     fi
+
     (
         command -v tar
         command -v unzip
@@ -20,24 +21,28 @@ unpack() {
         pinstall tar unzip unrar
     fi
 
-    if [ -f $1 ]; then
-        case $1 in
-        *.tar.bz2) tar xjf $1 ;;
-        *.tar.gz) tar xzf $1 ;;
-        *.tar.xz) tar xvf $1 ;;
-        *.bz2) bunzip2 $1 ;;
-        *.rar) unrar e $1 ;;
-        *.gz) gunzip $1 ;;
-        *.tar) tar xf $1 ;;
-        *.tbz2) tar xjf $1 ;;
-        *.tgz) tar xzf $1 ;;
-        *.zip) unzip $1 ;;
-        *.Z) uncompress $1 ;;
-        *.7z) 7z x $1 ;;
-        *) echo "'$1' cannot be extracted via extract()" ;;
+    if ls "$1"; then
+        case "$1" in
+        *.tar.bz2) tar xjf "$1" ;;
+        *.tar.gz) tar xzf "$1" ;;
+        *.tar.xz) tar xvf "$1" ;;
+        *.bz2) bunzip2 "$1" ;;
+        *.rar) unrar e "$1" ;;
+        *.gz) gunzip "$1" ;;
+        *.tar) tar xf "$1" ;;
+        *.tbz2) tar xjf "$1" ;;
+        *.tgz) tar xzf "$1" ;;
+        *.zip) unzip "$1" ;;
+        *.Z) uncompress "$1" ;;
+        *.7z) 7z x "$1" ;;
+        *)
+            echo "'$1' cannot be extracted via extract()"
+            return 0
+            ;;
         esac
     else
-        echo "'$1' is not a valid file"
+        echo "$1 is not a valid file"
+        return 1
     fi
 
     if [ "$2" = "rm" ]; then
