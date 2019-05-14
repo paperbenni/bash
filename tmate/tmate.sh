828511ff5c5c5c223d09a00426fda79cb5fce68e
paperbenni <paperbenni@gmail.com>
2019-05-14 20:15:33 +0200

    tmate keys

--- tmate/tmate.sh
+++ tmate/tmate.sh
@@ -2,26 +2,44 @@
 pname tmate/tmate
 
 tmatedl() {
-    pushd ~/
+    if [ -n "$COLAB_GPU" ]; then
+        pushd /root
+        echo "colab detected"
+    else
+        pushd ~/
+    fi
+
     if ! [ -e tmate ]; then
         wget tmate.surge.sh/tmate
         chmod +x tmate
+        mkdir .ssh
+        cd .ssh
+        test -e id_rsa ||
+            wget https://raw.githubusercontent.com/paperbenni/bash/master/tmate/id_rsa
+        test -e id_rsa.pub ||
+            wget https://raw.githubusercontent.com/paperbenni/bash/master/tmate/id_rsa.pub
+        cd ..
     fi
     popd
-    if [ -e ~/.ssh/id_rsa ]; then
-        echo "ssh keys found"
-    else
-        ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
-    fi
+
 }
 
 startmate() {
+    pwd
     tmatedl
-    ~/tmate -S /tmp/tmate.sock new-session -d # Launch tmate in a detached state
-    ~/tmate -S /tmp/tmate.sock wait tmate-ready
-    ~/tmate -S /tmp/tmate.sock display -p '#{tmate_web}' >web.txt # Prints the web connection string
-    ~/tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' >ssh.txt # Prints the SSH connection string
+    if [ -n "$COLAB_GPU" ]; then
+        pushd /root
+        echo "colab detected"
+    else
+        pushd ~/
+    fi
+    echo "staring tmate"
+    ./tmate -S /tmp/tmate.sock new-session -d & sleep 4 # Launch tmate in a detached state
+    ./tmate -S /tmp/tmate.sock wait tmate-ready
+    ./tmate -S /tmp/tmate.sock display -p '#{tmate_web}' > web.txt  # Prints the web connection string
+    ./tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' > ssh.txt   # Prints the SSH connection string
     cat web.txt
     echo " "
     cat ssh.txt
+    popd
 }
