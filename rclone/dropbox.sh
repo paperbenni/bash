551758906ad4a69d9e1b5b8e1f92e2c7bf46b9ee
paperbenni <paperbenni@gmail.com>
2019-06-18 23:33:15 +0200

    optional addbox args

--- rclone/dropbox.sh
+++ rclone/dropbox.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 pname rclone/dropbox
 addbox() {
-    if [ -z "$1" ] || [ -z "$2" ]; then
-        echo "usage: addbox name token"
+    if [ -z "$1" ]; then
+        echo "usage: addbox [name] token"
         return 0
     fi
+
     pb replace
     mkdir ~/.config/rclone
     pushd ~/.config/rclone
     curl https://raw.githubusercontent.com/paperbenni/bash/master/rclone/conf/dropbox.conf >>rclone.conf
-    rpstring "dropname" "$1" rclone.conf
-    rpstring "droptoken" "$2" rclone.conf
+    if [ -z "$2" ]; then
+        rpstring "dropname" "dropbox" rclone.conf
+        rpstring "droptoken" "$1" rclone.conf
+    else
+        rpstring "dropname" "$1" rclone.conf
+        rpstring "droptoken" "$2" rclone.conf
+    fi
     popd
 }
