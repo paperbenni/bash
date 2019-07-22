f64b5266fee9bd8f4ad59b41e64dc3244dd46b31
paperbenni <paperbenni@gmail.com>
2019-07-22 18:06:31 +0200

    ghrepos

--- git/git.sh
+++ git/git.sh
@@ -4,3 +4,8 @@ pname git/git
 gitfolder() {
     svn export https://github.com/"$1".git/trunk/"$2"
 }
+
+ghrepos() {
+    GHUSER="$1"
+    curl "https://api.github.com/users/$GHUSER/repos?per_page=100" | grep -o 'git@[^"]*'
+}
