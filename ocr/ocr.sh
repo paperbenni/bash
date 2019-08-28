30fcc84ac46da2d8414c9dbccf6db5f5206ad244
paperbenni <paperbenni@gmail.com>
2019-08-28 16:18:31 +0200

    ocr checks

--- ocr/ocr.sh
+++ ocr/ocr.sh
@@ -4,9 +4,11 @@
 pname ocr/ocr
 
 ocr() {
-    if command -v tesseract && [ -e "$1" ]; then
+    if command -v tesseract &> /dev/null && [ -e "$1" ]; then
         convert -colorspace gray -fill white -resize 480% -sharpen 0x1 "$1" "tes$1" || return 1
-        tesseract "tes$1" "${1%.*}"
+        tesseract "tes$1" "${1%.*}" &> /dev/null
+        cat "${1%.*}.txt"
+        rm "${1%.*}.txt" "tes$1"
     else
         echo "usage: ocr file"
     fi
