685183ca72b8e20a99bd96fac2910bbfdd3c852c
paperbenni <paperbenni@gmail.com>
2019-10-14 13:30:05 +0200

    dpop

--- dialog/dmenu.sh
+++ dialog/dmenu.sh
@@ -4,7 +4,7 @@ pname dialog/dmenu
 dconfirm() {
     echo "yes" >~/.doptions
     echo "no" >>~/.doptions
-    CHOICE=$(dmenu < ~/.doptions)
+    CHOICE=$(dmenu <~/.doptions)
     case "$CHOICE" in
     yes)
         return 0
@@ -18,6 +18,15 @@ dconfirm() {
 dsudo() {
     test -n "$SUDOPASS" || SUDOPASS=$(echo '' | dmenu -P -p "sudo password")
     printf "$SUDOPASS" | sudo -S "$@"
+}
+
+dpop() {
+    yres=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/' | egrep -o '[^x]*$')
+    echo 'ok' | dmenu -y $(expr $yres / 2) -p "$1" -l 10 &
+    sleep 5
+    if pgrep dmenu; then
+        pkill dmenu
+    fi
 }
 
 dtext() {
