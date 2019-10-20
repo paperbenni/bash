27c8fcfeb8231768fac224dc8beb3f5c6b514774
paperbenni <paperbenni@gmail.com>
2019-10-20 12:31:54 +0200

    checkexit

--- bash/bash.sh
+++ bash/bash.sh
@@ -114,6 +114,15 @@ function zerocheck() {
     done
 }
 
+function checkexit() {
+    if [ $? -eq 0 ]; then
+        echo 'OK'
+    else
+        echo 'FAIL'
+        exit 1
+    fi
+}
+
 phelp() {
     if [ "$1" = "--help" ]; then
         echo "$2"
