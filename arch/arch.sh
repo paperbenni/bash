d55f6400d86c3e74d575b9caf5f64a77070de07f
terminalforlife <terminalforlife@yahoo.com>
2019-11-03 20:57:59 +0000

    Updated arch.sh; used subshell instead

--- arch/arch.sh
+++ arch/arch.sh
@@ -6,30 +6,31 @@ aurinstall() {
         echo "your os doesn't support the AUR" >&2
         return 1
     fi
-    pushd . &>/dev/null
-    cd
-    mkdir -p .cache/aur
-    cd .cache/aur
-    if ! [ -e "$1" ]; then
-        git clone "https://aur.archlinux.org/$1.git"
-        cd "$1"
-    else
-        cd "$1"
-        git reset --hard
-        git pull
-    fi
-
-    for i in $(grep 'provides' <.SRCINFO | grep -Eo '=.*' | grep -Eo '[^ =]*'); do
-        if command -v $i; then
-            echo "program already installed"
-            return 0
+    #pushd . &>/dev/null
+    (
+        cd
+        mkdir -p .cache/aur
+        cd .cache/aur
+        if ! [ -e "$1" ]; then
+            git clone "https://aur.archlinux.org/$1.git"
+            cd "$1"
+        else
+            cd "$1"
+            git reset --hard
+            git pull
         fi
-    done
-
-    makepkg .
-    sudo pacman -U --noconfirm ./*.pkg.tar.xz
-    cd ..
+    
+        for i in $(grep 'provides' <.SRCINFO | grep -Eo '=.*' | grep -Eo '[^ =]*'); do
+            if command -v $i; then
+                echo "program already installed"
+                return 0
+            fi
+        done
+    
+        makepkg .
+        sudo pacman -U --noconfirm ./*.pkg.tar.xz
+    )
     rm -rf "$1"
 
-    popd &>/dev/null
+    #popd &>/dev/null
 }
