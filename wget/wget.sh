b4b46008c0dab461f1c9c691e5ef3c6f96c94f30
paperbenni <paperbenni@gmail.com>
2019-11-03 11:10:45 +0100

    downloadimages jpeg, reformat

--- wget/wget.sh
+++ wget/wget.sh
@@ -2,10 +2,18 @@
 pname wget/wget
 pb wget/fakebrowser
 
+downloadformat() {
+    if grep "$1" <index.html; then
+        wget -i $(egrep -o 'src=".*/.*\.'"$1"'"' <index.html | egrep -o '".*"' | egrep -o '[^"]*')
+    fi
+}
+
 # download all images from a single webpage
 downloadimages() {
     fakebrowser "$1"
-    wget -i $(egrep -o 'src=".*/.*\.jpg"' <index.html | egrep -o '".*"' | egrep -o '[^"]*')
-    wget -i $(egrep -o 'src=".*/.*\.png"' <index.html | egrep -o '".*"' | egrep -o '[^"]*')
+    downloadformat "jpg"
+    downloadformat "png"
+    downloadformat "jpeg"
+    downloadformat "gif"
     rm index.html
 }
