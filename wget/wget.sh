ea2795fe2559cd10549f6b32cab3c44ac15fe0a1
paperbenni <paperbenni@gmail.com>
2019-11-03 10:58:42 +0100

    fix downloadimages

--- wget/wget.sh
+++ wget/wget.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pname wget/wget
 
-# download all images from webpage
+# download all images from a single webpage
 downloadimages() {
-    wget -i $(curl "$1" | egrep -o 'src=".*/.*.jpg"' | egrep -o '".*"' | egrep -o '[^"]*')
+    wget -i $(curl "$1" | egrep -o 'src=".*/.*\.jpg"' | egrep -o '".*"' | egrep -o '[^"]*')
 }
