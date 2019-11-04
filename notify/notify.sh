c97f1d097e097ff9df3221396e5f130c1fecf1b5
terminalforlife <terminalforlife@yahoo.com>
2019-11-04 17:54:48 +0000

    Updated notify.sh; logic clean-up & optimization

--- notify/notify.sh
+++ notify/notify.sh
@@ -3,17 +3,6 @@
 pname notify/notify
 
 buttonset() {
-    if [ -n "$2" ]; then
-        if [ "$2" = "0" ]; then
-            BUTTONSTATE=false
-        else
-            if [ "$2" = "1" ]; then
-                BUTTONSTATE=true
-            fi
-        fi
-    else
-        BUTTONSTATE=false
-    fi
-
-    notify-send.py a --hint boolean:deadd-notification-center:true int:id:"$1" boolean:state:"$BUTTONSTATE"
+    [ "$2" = "1" ] && BUTTONSTATE=true
+    notify-send.py a --hint boolean:dead-notification-center:true int:id:"$1" boolean:state:"${BUTTONSTATE:-false}"
 }
