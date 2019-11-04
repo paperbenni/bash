39ce393f5e0a77712070bfc3a6147abec8001f62
paperbenni <paperbenni@gmail.com>
2019-11-04 10:51:31 +0100

    downloadimages for non-index.html

--- mud/mud.sh
+++ mud/mud.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pb mud/mud
-#opens the mud launcer from github
+#opens the mud launcher from github
 mud() {
     curl https://raw.githubusercontent.com/paperbenni/mud-launcher/master/launch.sh | bash
 }
--- wget/wget.sh
+++ wget/wget.sh
@@ -4,16 +4,23 @@ pb wget/fakebrowser
 
 downloadformat() {
     if grep "$1" <index.html; then
+        INDEX="${2:-index.html}"
         wget -i $(egrep -o 'src=".*/.*\.'"$1"'"' <index.html | egrep -o '".*"' | egrep -o '[^"]*')
     fi
 }
 
 # download all images from a single webpage
 downloadimages() {
+    mkdir .imagecache
+    cd .imagecache
     fakebrowser "$1"
-    downloadformat "jpg"
-    downloadformat "png"
-    downloadformat "jpeg"
-    downloadformat "gif"
-    rm index.html
+    SINDEX="$(ls)"
+    downloadformat "jpg" "$SINDEX"
+    downloadformat "png" "$SINDEX"
+    downloadformat "jpeg" "$SINDEX"
+    downloadformat "gif" "$SINDEX"
+    rm "$SINDEX"
+    mv ./* ../
+    cd ..
+    rm -r .imagecache
 }
