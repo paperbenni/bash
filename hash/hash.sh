6e9d0624f8f63c8ec44763c2b9eece2524b5f3aa
terminalforlife <terminalforlife@yahoo.com>
2019-11-14 23:07:10 +0000

    Prefer cut over awk for lighter processing
    With `awk` being a programming language in its own right, I believe
    using `cut` should be much more efficient.

--- hash/hash.sh
+++ hash/hash.sh
@@ -2,14 +2,12 @@
 pname hash/hash
 
 different() {
-    HASH1=$(sha256sum $1 | awk '{ print $1 }')
-    HASH2=$(sha256sum $2 | awk '{ print $1 }')
+    HASH1=$(sha256sum "$1" | cut -d ' ' -f 1)
+    HASH2=$(sha256sum "$2" | cut -d ' ' -f 1)
     if [ "$HASH1" = "$HASH2" ]; then
-        echo "files $1 and $2 are identical"
+        echo "files '$1' and '$2' are identical"
         return 1
     else
-        return 0
-        echo "files $1 and $2 differ"
+        echo "files '$1' and '$2' differ"
     fi
-
 }
