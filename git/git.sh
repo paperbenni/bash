c025182d663ac4598a2c50b4568891aa876db9ed
paperbenni <paperbenni@gmail.com>
2019-11-30 11:26:00 +0100

    improve gh backup

--- git/git.sh
+++ git/git.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 pname git/git
 
+# uses svn to download a single folder from github
 gitfolder() {
     svn export https://github.com/"$1".git/trunk/"$2"
 }
 
+# get a list of all the public repos of a user
 ghrepos() {
-    GHUSER="$1"
+    GHUSER="${1:-paperbenni}"
     curl -s "https://api.github.com/users/$GHUSER/repos?per_page=100" | grep -E -o 'git@[^"]*' | grep -o ':.*' | grep -E -o '[^:]*'
 }
 
 ghbackup() {
-    for repo in $(ghrepos $1); do
-        git clone "https://github.com/$repo"
+    if [ -n "$2" ] && [ "$2" -eq "$2" ]; then
+        GITDEPTH="$2"
+    fi
+    for repo in $(ghrepos ${1:-paperbenni}); do
+        REPONAME=$(grep -o '/.*\.' <<<$repo)
+        if [ -d $REPONAME ]; then
+            cd $REPONAME
+            git pull
+            cd ..
+        else
+            if [ -n "$GITDEPTH" ]; then
+                git clone --depth="$GITDEPTH" "https://github.com/$repo"
+            else
+                git clone "https://github.com/$repo"
+            fi
+        fi
     done
 }
 
@@ -24,13 +40,13 @@ papergit() {
 # clone repo, default to github, default to my username
 gitclone() {
     zerocheck "$1"
-    if grep ':' <<<"$1"; then
+    if grep -q '://' <<<"$1"; then
         git clone --depth=1 "$1"
     else
-        if grep '/' <<<"$1"; then
+        if grep -q '/' <<<"$1"; then
             git clone --depth=1 "https://github.com/$1.git"
         else
-            git clone --depth 1 "https://github.com/paperbenni/$1"
+            git clone --depth 1 "https://github.com/paperbenni/$1.git"
         fi
     fi
 }
