c083d83a7f80aa1f2797d9dd62f1168ffe739d45
paperbenni <paperbenni@gmail.com>
2020-02-03 23:40:12 +0100

    instanttheme offline

--- instantos/instantos.sh
+++ instantos/instantos.sh
@@ -6,8 +6,12 @@ pname instantos
 instanttheme() {
     [ -n "$1" ] || return
     mkdir -p ~/instantos/themes &>/dev/null
-    curl -s "https://raw.githubusercontent.com/instantOS/instant-THEMES/master/colors/$1.theme" > \
-        ~/instantos/themes/"$1.theme"
+    if [ -e /usr/share/instantthemes ]; then
+        cat /usr/share/instantthemes/colors/$1.theme >~/instantos/themes/"$1.theme"
+    else
+        curl -s "https://raw.githubusercontent.com/instantOS/instant-THEMES/master/colors/$1.theme" > \
+            ~/instantos/themes/"$1.theme"
+    fi
     echo "$1" >~/instantos/themes/config
 }
 
