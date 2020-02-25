ac8a25a5eff1aed8ee90cd2006c08f237607613b
paperbenni <paperbenni@gmail.com>
2020-02-25 13:55:58 +0100

    first working version of pyenv

--- python/python.sh
+++ python/python.sh
@@ -19,33 +19,58 @@ installpip() {
 
 # download and compile a python version to use in a virtualenv
 pyenv() {
-    if [ -n "$@" ]; then
+
+    # scrape version numbers from website
+    if [ -z "$@" ]; then
         echo "list of python versions"
-        curl -s https://www.python.org/downloads | grep -o 'Python [0-9]\.[0-9]\.[0-9]' | sort -u | tac
+        curl -s https://www.python.org/downloads/ | grep -o 'Python [0-9]\.[0-9]\.[0-9]' | sort -u | tac
         return
     fi
 
-    pushd .
-    cd
-    mkdir -p .cache/pyenv
-    cd .cache/pyenv
-    mkdir "$1"
-    cd "$1"
-    wget -O python.tar.xz https://www.python.org/ftp/python/$1/Python-$1.tar.xz
-
-    if ! unxz python.tar.xz; then
-        cd ..
-        rm -rf "$1"
-        popd
-        echo "error: python version not existing"
+    if ! grep -Eiq '(3|2|2)\.[0-9]\.[0-9]' <<<"$1"; then
+        echo "please specify a python version number"
         return 1
     fi
 
-    tar xvf python.tar
-    cd Python-*
-    ./configure --prefix="$(pwd)"/mybuild
-    make
-    make install
+    # download and compile if the python version is not installed
+    if ! [ -e ~/.cache/pyenv/$1/mybuild ]; then
+        pushd .
+        cd
+        mkdir -p .cache/pyenv
+        cd .cache/pyenv
+        mkdir "$1"
+        cd "$1"
+        wget -O python.tar.xz https://www.python.org/ftp/python/$1/Python-$1.tar.xz
 
-    popd
+        if ! unxz python.tar.xz; then
+            cd ..
+            rm -rf "$1"
+            popd
+            echo "error: python version not existing"
+            return 1
+        fi
+
+        tar xvf python.tar
+        PYPATH="$(pwd)"
+        cd Python-*
+        ./configure --prefix="$PYPATH"/mybuild
+        make
+        make install
+        popd
+    fi
+
+    # create virtualenv and source activation script
+    if [ -e ./bin/activate ]; then
+        echo "virtualenv already found"
+        source ./bin/activate
+    else
+        if [ -e "$HOME"/.cache/pyenv/$1/mybuild/bin/python3 ]; then
+            virtualenv --python "$HOME"/.cache/pyenv/$1/mybuild/bin/python3
+        elif [ -e "$HOME"/.cache/pyenv/$1/mybuild/bin/python2 ]; then
+            virtualenv --python "$HOME"/.cache/pyenv/$1/mybuild/bin/python3
+        else
+            echo "error: no python binary in "$HOME"/.cache/pyenv/$1/mybuild/bin/"
+        fi
+    fi
+
 }
