9d9e1ad9c96e9ef19b3be7a9f0e55ce895d02314
paperbenni <paperbenni@gmail.com>
2020-03-30 16:50:46 +0200

    add passwordbox

--- dialog/dialog.sh
+++ dialog/dialog.sh
@@ -7,12 +7,23 @@ confirm() {
 }
 
 textbox() {
-    DIATEXT=${1:-enter text}
     unset user_input
     while [ -z "$user_input" ]; do
         #statements
         user_input=$(
-            dialog --inputbox "$DIATEXT" 8 40 \
+            dialog --inputbox "${1:-enter text}" 8 40 \
+                3>&1 1>&2 2>&3 3>&-
+        )
+    done
+    echo "$user_input"
+}
+
+passwordbox() {
+    unset user_input
+    while [ -z "$user_input" ]; do
+        #statements
+        user_input=$(
+            dialog --passwordbox "${1:-enter password}" 8 40 \
                 3>&1 1>&2 2>&3 3>&-
         )
     done
