231a73bfefde8baabaeaef56b966fc2d6ea2451b
paperbenni <paperbenni@gmail.com>
2020-09-27 11:42:17 +0200

    fix permainstall

--- permainstall.sh
+++ permainstall.sh
@@ -6,8 +6,7 @@ cd || exit 1
 mkdir -p workspace
 cd workspace || exit 1
 git clone --depth=1 https://github.com/paperbenni/bash.git
-rm -rf bash/.git
-
+source ./bash/import.sh || exit 1
 cd || exit 1
 
 if grep -q 'workspace/bash/import.sh' ~/.bashrc
@@ -15,7 +14,7 @@ then
     echo "already installed"
     exit 
 fi
+
 appendmarkers 'source ~/workspace/bash/import.sh' .bashrc
-sudo appendmarkers 'source ~/workspace/bash/import.sh' /etc/profile
 
 echo 'offline' >~/.paperoff
